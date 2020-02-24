358af5ff2a2f36869b4c3650826e1062b1a1314b
brandon <leeyongl5263@gmail.com>
2020-02-24 10:00:00 -0800

    Test koala2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as tokens from './Sharable/tokens';
+import * as Button from './Button/index';
 
 export { tokens };
-
+export { Button };
 // export * from './Sharable/tokens';
-export * from './Button/index';
