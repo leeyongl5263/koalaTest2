6d75dae2718071c80d8d9b4c6168414018a45332
brandon <leeyongl5263@gmail.com>
2020-02-24 09:57:19 -0800

    test koala

---
+++ src/Button/LinkButton.style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import tokens from '../Sharable/tokens';
+
+
+const LinkWithUnderLine = `
+  text-decoration: underline;
+`;
+
+const LinkButtonStyle = styled.a`
+  font-family: ${tokens.fontFamily.default};
+  border-radius: ${tokens.border.radius};
+  border-width: 1px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-flex;
+  font-weight: bold;
+  font-size: 18px;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+    color: ${tokens.color.brandOrange};
+  }
+  ${props => (props.hasUnderLine ? LinkWithUnderLine : "")}
+`;
+
+
+
+
+export default LinkButtonStyle;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as tokens from './Sharable/tokens';
+
+export { tokens };
+
+// export * from './Sharable/tokens';
+export * from './Button/index';
